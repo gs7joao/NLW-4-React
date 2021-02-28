831a0d37f4cd19874f84d0af144396d3a70258aa
João Pedro <75731084+gs7joao@users.noreply.github.com>
2021-02-28 10:58:37 -0300

    NLW4 - Aula 01
    Criação do Projeto e ExperienceBar

---
+++ moveit/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// JS JavasScript 
+// JSX JavaScript Xml
+
+//Componentes tudo que conseguimos manipular
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
